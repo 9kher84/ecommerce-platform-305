2fa1ce2329ea0fe7308c920d2eeb410d025810a9
9kher84 <saeedaalshehri@gmail.com>
2026-06-20 12:44:01 +0300

    Fix syntax error in RequestDetailsPage.jsx IIFE closing tags

--- frontend/src/pages/RequestDetailsPage.jsx
+++ frontend/src/pages/RequestDetailsPage.jsx
@@ -1094,7 +1094,7 @@ const RequestDetailsPage = () => {
                         </div>
                       )}
                     </div>
-                  ))()}
+                  ))})()}
                 </div>
               )}
             </div>
